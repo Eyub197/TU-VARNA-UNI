c061b486f87e33695a8a52dcad982f69138be3b3
Eyub Mehmed Eyub <eyubmehmed122@gmail.com>
2026-02-20 15:02:30 +0200

    test

--- 1.3-algorithms-and-data-structures/1-lab-exercise.cpp
+++ 1.3-algorithms-and-data-structures/1-lab-exercise.cpp
@@ -1,3 +1,4 @@
 // I forgot to push it it was some sort of a matrix and for odd rows we were
-// finding the biggest num and for even rows we were finding the smallest ones and then printing in row x the smalles number is 
-// number then in row x the biggest number is number and so on
+// finding the biggest num and for even rows we were finding the smallest ones
+// and then printing in row x the smalles number is number then in row x the
+// biggest number is number and so on
