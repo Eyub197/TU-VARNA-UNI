922084c7ff8f30cc539c6bc6e2cb7f20770be75a
Eyub197 <eyubmehmed122@gmail.com>
2026-02-26 14:57:04 +0200

    Update 2-lab-exercise.cpp and 3-lab-exercise.cpp with new functionalities.

---
+++ 1.3-algorithms-and-data-structures/2-lab-exercise.cpp
@@ -0,0 +1,53 @@
+// декларирайте матрица а[20][20]
+// въведете цело число n (3 < n <= 20)
+// Запълнет мартрицата a[n][n] по следната локига
+//
+// следната логика : n = 4
+// | 4 | 4 | 4 | 4
+// | 0 | 3 | 3 | 3
+// | 0 | 0 | 2 | 2
+// | 0 | 0 | 0 | 1
+//
+// n = 5
+// 5 | 5 | 5 | 5 | 5
+// 0 | 4 | 4 | 4 | 4
+// 0 | 0 | 3 | 3 | 3
+// 0 | 0 | 0 | 2 | 2
+// 0 | 0 | 0 | 0 | 1
+
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
+
+int getN() {
+  int userInput;
+
+  do {
+    cout << "Enter a number n betaween 3 and 20: ";
+    cin >> userInput;
+  } while (userInput < 3 || userInput > 20);
+
+  return userInput;
+}
+
+void populateMatrix(int matrix[20][20], int n) {
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < n; ++j) {
+      if (j >= i) {
+        matrix[i][j] = n - i;
+      } else {
+        matrix[i][j] = 0;
+      }
+      cout << setw(4) << matrix[i][j] << " ";
+    }
+    cout << endl;
+  }
+}
+
+int main() {
+  int matrix[20][20];
+
+  const int n = getN();
+  populateMatrix(matrix, n);
+}
---
+++ 1.3-algorithms-and-data-structures/3-lab-exercise.cpp
@@ -0,0 +1,69 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+/**
+ * TU Varna - ASD / UP
+ * Problem: array shifting
+ */
+
+// Helper function to print raw arrays
+void printArray(int arr[], int n) {
+  for (int i = 0; i < n; i++) {
+    cout << arr[i] << (i == n - 1 ? "" : " ");
+  }
+  cout << endl;
+}
+
+void handlePositiveKValue() {}
+
+int main() {
+  const int n = 10;
+  int k = -3;
+  int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  int tempArr[k];
+  int secondTempArr[n - k];
+
+  // Start manual logic here
+
+  if (k > 0) {
+    for (int i = 0; i < k; i++) {
+      tempArr[i] = arr[n - k + i];
+    }
+
+    for (int i = 0; i < n - k; i++) {
+      secondTempArr[i] = arr[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+      if (i < k) {
+        arr[i] = tempArr[i];
+      } else {
+        arr[i] = secondTempArr[i - k];
+      }
+    }
+  } else {
+    k = k * -1;
+    for (int i = 0; i < k; i++) {
+      tempArr[i] = arr[k - i - 1];
+    }
+
+    for (int i = 0; i < n - k; i++) {
+      if (i < n) {
+        secondTempArr[i] = arr[i];
+        cout << secondTempArr[i];
+      }
+    }
+
+    for (int i = 0; i < n; i++) {
+      if (i < k) {
+        arr[i] = tempArr[i];
+      } else {
+        arr[i] = secondTempArr[i - k];
+      }
+    }
+  }
+
+  return 0;
+}
