733af99290162e753de1a7397669d16024ddb29e
Eyub197 <eyubmehmed122@gmail.com>
2026-03-05 14:31:11 +0200

    Add lab exercise for conditionals

---
+++ 1.2-basic-programming-2/3-Conditionals/3.Lab-exercise.py
@@ -0,0 +1,22 @@
+def calcPoints():
+    point_count = 82
+    bonus_points = 0
+    if point_count <= 80:
+        bonus_points = 3
+    elif point_count > 80:
+        bonus_points = point_count * 0.05
+    elif point_count > 90:
+        bonus_points = point_count * 0.10
+
+    if point_count % 2 == 0:
+        bonus_points += 1
+    elif point_count % 3 == 0:
+        bonus_points += 2
+
+    return point_count, bonus_points
+
+
+point_count, bonus_points = calcPoints()
+
+print(f"Bonus points {bonus_points:g}")
+print(f"All points {bonus_points + point_count}")
