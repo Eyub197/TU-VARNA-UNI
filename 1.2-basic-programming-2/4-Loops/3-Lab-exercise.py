ccf78dd7479dd89eabb4b5bfca0f7fd71eafc891
Eyub197 <eyubmehmed122@gmail.com>
2026-03-12 14:50:36 +0200

    fix: add code for computing the maximum difference between sums of two pairs of numbers

---
+++ 1.2-basic-programming-2/4-Loops/3-Lab-exercise.py
@@ -0,0 +1,12 @@
+first_number = 1
+second_number = 1
+third_number = 4
+fourth_number = 5
+
+first_duo = first_number + second_number
+second_duo = third_number + fourth_number
+
+if first_duo == second_duo:
+    print(f"Yes, value={first_duo}")
+else:
+    print(f"No, maxdiff={second_duo}")
